3ac73d4825f0714217fb6f06a4057eada2fe8879
Reagan Mackey <reaganpmackey@gmail.com>
2022-04-10 16:41:47 -0700

    Completed unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,57 +1,80 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Statistics = ({good, neutral, bad, total}) => {
+const Button = ({ title, handler }) => {
+  return <button onClick={handler}>{title}</button>;
+};
+
+const StatisticsTable = ({ good, neutral, bad, total }) => {
   if (total === 0) {
     return (
       <>
-      <h1>statistics</h1>
-      <p>No feedback given yet.</p>
+        <h1>statistics</h1>
+        <p>No feedback given yet.</p>
       </>
-    ) 
+    );
   }
+
   return (
     <>
-    <h1>statistics</h1>
-    <ul>
-      <li>{"good " + good}</li>
-      <li>{"neutral " + neutral}</li>
-      <li>{"bad " + bad}</li>
-      <li>{"total " + total}</li>
-      <li>{"average " + ((total === 0) ? 0 : (good - bad) / total)}</li>
-      <li>{"positive " + ((total === 0) ? 0 + "%" : (good / total) * 100 + "%")}</li>
-    </ul>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>total</td>
+            <td>{total}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{total === 0 ? 0 : (good - bad) / total}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td>{total === 0 ? 0 + "%" : (good / total) * 100 + "%"}</td>
+          </tr>
+        </tbody>
+      </table>
     </>
-  )
-}
+  );
+};
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
   const handleClick = (title) => {
     if (title === "good") {
-      setGood(good + 1)
+      setGood(good + 1);
+    } else if (title === "neutral") {
+      setNeutral(neutral + 1);
+    } else if (title === "bad") {
+      setBad(bad + 1);
     }
-    else if (title === "neutral") {
-      setNeutral(neutral + 1)
-    }
-    else if (title === "bad") {
-      setBad(bad + 1)
-    }
-    setTotal(total + 1)
-  }
+    setTotal(total + 1);
+  };
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => handleClick("good")}>good</button>
-      <button onClick={() => handleClick("neutral")}>neutral</button>
-      <button onClick={() => handleClick("bad")}>bad</button>
-      <Statistics good={good} neutral={neutral} bad={bad} total={total} />
+      <Button title="good" handler={() => handleClick("good")} />
+      <Button title="neutral" handler={() => handleClick("neutral")} />
+      <Button title="bad" handler={() => handleClick("bad")} />
+      <StatisticsTable good={good} neutral={neutral} bad={bad} total={total} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
