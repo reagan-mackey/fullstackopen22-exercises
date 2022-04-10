4d4bce2979c9b260c17cfb24eba8f7715d537d18
Reagan Mackey <reaganpmackey@gmail.com>
2022-04-10 14:28:08 -0700

    Completed unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,17 +10,34 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const handleClick = (title) => {
+    if (title === "good") {
+      setGood(good + 1)
+    }
+    else if (title === "neutral") {
+      setNeutral(neutral + 1)
+    }
+    else if (title === "bad") {
+      setBad(bad + 1)
+    }
+    setTotal(total + 1)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={() => handleClick("good")}>good</button>
+      <button onClick={() => handleClick("neutral")}>neutral</button>
+      <button onClick={() => handleClick("bad")}>bad</button>
       <h1>statistics</h1>
       <Display value={good} title="good" />
       <Display value={neutral} title="neutral" />
       <Display value={bad} title="bad" />
+      <Display value={total} title="total" />
+      <Display value={(total === 0) ? 0 : (good - bad) / total} title="average" />
+      <Display value={(total === 0) ? 0 + "%" : (good / total) * 100 + "%"} title="positive" />
     </div>
   )
 }
