ee04c7854af5aeaa5fda1dfc8384a811eed45809
Reagan Mackey <reaganpmackey@gmail.com>
2022-04-15 14:31:15 -0700

    Completed phonebook step 1

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+    const nameObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(nameObject));
+    setNewName("");
+  };
+
+  const handleChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <table>
+        <tbody>
+          {persons.map((person) => (
+            <>
+              <tr>
+                <td key={person.name}>{person.name}</td>
+              </tr>
+            </>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
